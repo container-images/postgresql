f8a72ba8048407be8494609d6e4a35e18ffafccc
Nils Philippsen <nils@redhat.com>
2017-05-15 17:24:30 +0200

    don't use nss_wrapper.so
    We don't yet have nss_wrapper in a module.

--- root/usr/share/container-scripts/postgresql/common.sh
+++ root/usr/share/container-scripts/postgresql/common.sh
@@ -131,7 +131,7 @@ function generate_passwd_file() {
   export GROUP_ID=$(id -g)
   grep -v ^postgres /etc/passwd > "$HOME/passwd"
   echo "postgres:x:${USER_ID}:${GROUP_ID}:PostgreSQL Server:${HOME}:/bin/bash" >> "$HOME/passwd"
-  export LD_PRELOAD=libnss_wrapper.so
+  #export LD_PRELOAD=libnss_wrapper.so
   export NSS_WRAPPER_PASSWD=${HOME}/passwd
   export NSS_WRAPPER_GROUP=/etc/group
 }
